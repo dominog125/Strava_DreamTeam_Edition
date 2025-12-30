41d8efb407c68a6d4a4efae3f896d83b841c90cb
DawidBrozyna <dawid.brozyna@studenci.collegiumwitelona.pl>
2025-12-30 17:12:12 +0100

    Mobile_1

---
+++ Mobile/mini_strava/lib/theme/app_colors.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Brand (Orange)
+  static const orange500 = Color(0xFFF97316);
+  static const orange600 = Color(0xFFEA580C);
+  static const orange700 = Color(0xFFC2410C);
+  static const orange400 = Color(0xFFFB923C);
+
+  // Grays
+  static const gray50  = Color(0xFFF9FAFB);
+  static const gray100 = Color(0xFFF3F4F6);
+  static const gray200 = Color(0xFFE5E7EB);
+  static const gray300 = Color(0xFFD1D5DB);
+  static const gray400 = Color(0xFF9CA3AF);
+  static const gray500 = Color(0xFF6B7280);
+  static const gray600 = Color(0xFF4B5563);
+  static const gray800 = Color(0xFF1F2937);
+  static const gray900 = Color(0xFF111827);
+
+  // Input bg
+  static const inputLight = Colors.white;
+  static const inputDark = Color(0xCC111827);
+
+  // Shadow orange 500 / 40%
+  static const orangeShadow40 = Color(0x66F97316);
+}

15295e20c823b31d749e8f1641fd4087fde2d72a
dominog125 <76757727+dominog125@users.noreply.github.com>
2026-01-15 12:14:52 +0100

    ActivityGpsPoint Model And Activity controller update

---
+++ API/Strava_DreamTeam_Edition_API/Models/DTO/AddGpsPointRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Strava_DreamTeam_Edition_API.Models.DTO
+{
+    public class AddGpsPointRequestDto
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
--- API/Strava_DreamTeam_Edition_API/obj/Debug/net8.0/Strava_DreamTeam_Edition_API.AssemblyInfo.cs
+++ API/Strava_DreamTeam_Edition_API/obj/Debug/net8.0/Strava_DreamTeam_Edition_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Strava_DreamTeam_Edition_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1025c51fa6cc5f0c17b408f4c56b0761bd650c64")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a0d36d3d59161372e9a1abe1c211e2f13f2c88e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Strava_DreamTeam_Edition_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Strava_DreamTeam_Edition_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

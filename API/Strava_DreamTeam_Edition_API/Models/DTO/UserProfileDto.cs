ebc16c7c103d871c99821632cc36dff039791138
dominog125 <76757727+dominog125@users.noreply.github.com>
2026-01-17 17:42:36 +0100

    UserProfile + FriendRealtion

---
+++ API/Strava_DreamTeam_Edition_API/Models/DTO/UserProfileDto.cs
@@ -0,0 +1,18 @@
+﻿namespace Strava_DreamTeam_Edition_API.Models.DTO
+{
+    public class UserProfileDto
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public string? Gender { get; set; }
+        public decimal? HeightCm { get; set; }
+        public decimal? WeightKg { get; set; }
+    
+    }
+
+    public class UpdateUserProfileRequest : UserProfileDto
+    {
+
+    }
+}
---
+++ API/Strava_DreamTeam_Edition_API/Models/Domain/UserProfile.cs
@@ -0,0 +1,28 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Strava_DreamTeam_Edition_API.Models.Domain
+{
+    public class UserProfile
+    {
+        [Key]
+        public string UserId { get; set; } = default!;
+
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime? BirthDate { get; set; }
+
+
+        public string? Gender { get; set; }
+
+      
+        public decimal? HeightCm { get; set; }
+        public decimal? WeightKg { get; set; }
+
+        public byte[]? Avatar { get; set; }
+        public string? AvatarContentType { get; set; }
+
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    }
+}

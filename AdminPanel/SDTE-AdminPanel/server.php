c0653d51ee66cdc8e6de96df9dcc4cfbb7379926
Pigus1234 <rudykamil.rudowski@gmail.com>
2026-01-18 11:45:23 +0100

    Docker / Serwer

--- AdminPanel/SDTE-AdminPanel/server.php
+++ AdminPanel/SDTE-AdminPanel/server.php
@@ -1,9 +1,10 @@
 <?php
 
+$publicPath = '/app/public';
 $uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) ?? '/');
 
-if ($uri !== '/' && file_exists(__DIR__ . '/public' . $uri)) {
+if ($uri !== '/' && file_exists($publicPath . $uri)) {
     return false;
 }
 
-require __DIR__ . '/public/index.php';
+require $publicPath . '/index.php';
